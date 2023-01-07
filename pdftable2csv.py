a9f2e78273104c46837ac02a3c87cb97b3f8c505
abrar Hussain <partuki.abrar@gmail.com>
2023-01-07 14:12:10 +0500

    tumhe yad rkhne k lye

---
+++ pdftable2csv.py
@@ -0,0 +1,20 @@
+import tabula
+import pandas as pd
+# Read the PDF file into a list of pandas DataFrames
+df_list = tabula.read_pdf("TableData.pdf", multiple_tables=True, pages="all")
+
+# Loop through the list of DataFrames
+for i, df in enumerate(df_list):
+  # Drop the 'Unnamed' column from each DataFrame
+  df = df.drop(columns=['Unnamed: 0'])
+
+# Concatenate the modified DataFrames into a single DataFrame
+df = pd.concat(df_list)
+
+#remove colums with no data 
+df = df.dropna(how='all', axis=1)
+
+# Save the combined DataFrame to a CSV file
+df.to_csv("modified_data.csv", index=False)
+
+
